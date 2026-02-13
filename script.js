cee4a878cd26dc243ef1ebb5afbd547c44c30718
Phoeniewenie <143308444+Phoeniewenie@users.noreply.github.com>
2026-02-14 01:24:15 +1100

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,6 @@ const button = document.getElementById("valentinesButton");
 button.addEventListener("click", () => {
   if (button.textContent === "Click Me! ❤") {
     button.textContent = "loading...";
-    fetch('send_mail.php')
       .then(response => {
         if (response.ok) {
           button.textContent = "🦧🦧🦧🦧";
